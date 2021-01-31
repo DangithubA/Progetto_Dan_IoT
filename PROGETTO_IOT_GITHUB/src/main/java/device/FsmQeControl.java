7a24050e9bcea4faa9ebaaac8f8fd692f7ee4799
DangithubA <262227@studenti.unimore.it>
2021-01-31 17:55:34 +0100

    aggiunta classe FsmQeControl

---
+++ PROGETTO_IOT_GITHUB/src/main/java/device/FsmQeControl.java
@@ -0,0 +1,18 @@
+package device;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class FsmQeControl {
+
+    private final ObjectMapper objectMapper;
+
+
+    public FsmQeControl(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+
+
+
+
+}
